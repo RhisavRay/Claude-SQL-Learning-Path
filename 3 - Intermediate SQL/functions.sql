ccf0f0598cf6aba884d5fa6f8c22de97ca6fd7b6
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-04-20 11:41:38 +0000

    Theory on String functions

--- 3 - Intermediate SQL/functions.sql
+++ 3 - Intermediate SQL/functions.sql
@@ -6,3 +6,24 @@ row individually.
 
 We'll cover five categories, moving fast since most of these are intuitive once you see them.
 */
+
+
+
+
+/*
+String functions
+
+The ones you'll use most often in DA work:
+
+Function                        |        What it does
+----------------------------------------------------------------------------------
+CONCAT(a, b)                    |        Joins strings together
+UPPER(s) / LOWER(s)             |        Case conversion
+LENGTH(s)                       |        Character count
+TRIM(s)                         |        Removes leading/trailing spaces
+SUBSTRING(s, start, length)     |        Extracts part of a string
+REPLACE(s, from, to)            |        Replaces occurrences of a substring
+LIKE                            |        Pattern matching (you know this one)
+
+Quick examples on our data:
+*/
